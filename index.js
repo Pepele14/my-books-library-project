1cd397afda86ff1a9e81422b17af6d35c0ec1b50
Giuseppe Moschetta <giu.mos.dev@gmail.com>
2024-01-29 01:15:10 +0100

    DB and client/server communication  updates

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
-
+import db from "./config.js"; // Import the db object from config.js
 
 const app = express();
 const port = 3000;
+db.connect();
 
-//db.connect();
 
-let books = [
-  { id: 1, title: "Lepanto", author:"Alessandro Barbero", completed: "true",  type: "History", rating: 5, recency: "2023" },
-
-];
+let listBooks = [];
+let myLibraryButtonClicked = false;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
 app.get("/", (req, res) => { 
-  res.render("index.ejs");
+  res.render("index.ejs", {
+    listBooks: listBooks,
+    myLibraryButtonClicked: myLibraryButtonClicked,
+  });
 });
 
-app.get("/myLibrary", async(req, res) => {  
-  try {
-    const allBooks = await db.query("SELECT * FROM books ORDER BY id ASC");
-    books = allBooks.rows;
 
-    res.render("index.ejs", {
-      listBooks: books
-    });
-} catch (err) {
-  console.log(err);
-}
+app.get("/myLibrary", async (req, res) => {  
+  try {
+    if (!myLibraryButtonClicked) {
+      //myLibraryButtonClicked = true;
+      const allBooks = await db.query("SELECT * FROM books ORDER BY id ASC");
+      const booksFromDB = allBooks.rows;
+
+      
+
+      res.render("index.ejs", {
+        listBooks: booksFromDB, 
+        myLibraryButtonClicked: myLibraryButtonClicked,
+      });
+    } else {
+      res.render("index.ejs", {
+        listBooks: [], // Empty array or handle accordingly if the list has already been displayed
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 
